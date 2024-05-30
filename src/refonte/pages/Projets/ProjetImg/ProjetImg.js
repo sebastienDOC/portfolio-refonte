6d25f2ebc29a36c472e59e7e1271378f9ba24f4e
sebastienDOC <s.dechand21@gmail.com>
2024-05-30 12:05:32 +0200

    img projets.

--- src/refonte/pages/Projets/ProjetImg/ProjetImg.js
+++ src/refonte/pages/Projets/ProjetImg/ProjetImg.js
@@ -4,11 +4,6 @@ import OC from '../../../../assets/logo/oc.webp'
 import Logo from '../../../../assets/logo/logo2.webp'
 
 const ProjetImg = ({cover, alt, id, type}) => {
-  // Récupération des images 
-  function getImgCover(img) {
-    let images = require("../../../../assets/projets/formation" + img);
-    return images;
-  }
   return (
       <div id={id} className='projets-img-ctn'>
         <div className='projets-logo-ctn'>
@@ -18,7 +13,7 @@ const ProjetImg = ({cover, alt, id, type}) => {
                 <img src={Logo} alt="Logo de Sébastien Dechand" className='projets-logo' title='Projet Professionnel / Personnel'/> 
             }
         </div>
-        <img src={getImgCover(cover)} alt={alt} style={{ display: 'block' }}/>
+        <img src={cover} alt={alt} style={{ display: 'block' }} preload/>
       </div>
   )
 }
